54f30987047c35389c227453e9438ff65658f982
Max Elveberg Nika <max@host-193-11-184-148.eduroam.bth.se>
2018-05-16 12:23:10 +0200

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Max Elveberg Nika on 2018-05-16.
+//  Copyright © 2018 Max Elveberg Nika. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
